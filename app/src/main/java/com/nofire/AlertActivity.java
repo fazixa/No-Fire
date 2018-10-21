21afd81a5665b1510e984c0e7db2131414ca38d4
Behzad Khanlar <behkha@yahoo.com>
2018-10-21 19:57:15 +0330

    permission added to alert activity

--- app/src/main/java/com/nofire/AlertActivity.java
+++ app/src/main/java/com/nofire/AlertActivity.java
@@ -1,19 +1,32 @@
 package com.nofire;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 
-import com.nofire.Adapter.TwiterAdapter;
-import com.nofire.Item.Twit_Item;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class AlertActivity extends AppCompatActivity {
 
@@ -36,32 +49,8 @@ public class AlertActivity extends AppCompatActivity {
         ivCaptured = this.findViewById(R.id.iv_captured);
         spnIncidentType = this.findViewById(R.id.spn_incident_type);
 
-        final String dir = Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES) + "/picFolder/";
-        File newdir = new File(dir);
-        newdir.mkdirs();
-        captureButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Here, the counter will be incremented each time, and the
-                // picture taken by camera will be stored as 1.jpg,2.jpg
-                // and likewise.
-                count++;
-                String file = dir + count + ".jpg";
-                File newfile = new File(file);
-                try {
-                    newfile.createNewFile();
-                } catch (IOException e) {
-                }
 
-                Uri outputFileUri = Uri.fromFile(newfile);
-
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-
-                startActivityForResult(cameraIntent, TAKE_PHOTO_CODE);
-            }
-        });
+        captureButton.setOnClickListener(view -> onCaptureCreated());
 
 
         incidentTypeList = new ArrayList<>();
@@ -84,5 +73,57 @@ public class AlertActivity extends AppCompatActivity {
             Bitmap photo = (Bitmap) data.getExtras().get("data");
             ivCaptured.setImageBitmap(photo);
         }
+    }
+
+    private void onCaptureCreated(){
+        if (hasPermission())
+             capture();
+        else
+            requestPermission();
+    }
+
+    private void requestPermission(){
+        Dexter.withActivity(this)
+                .withPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        capture();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
+    }
+
+    private void capture(){
+        final String dir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES) + "/picFolder/";
+        File newdir = new File(dir);
+        newdir.mkdirs();
+        count++;
+        String file = dir + count + ".jpg";
+        File newfile = new File(file);
+        try {
+            newfile.createNewFile();
+        } catch (IOException e) {
+        }
+
+        Uri outputFileUri = Uri.fromFile(newfile);
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+        startActivityForResult(cameraIntent, TAKE_PHOTO_CODE);
+    }
+
+    private boolean hasPermission(){
+        String writePermission = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+        String readPermission = Manifest.permission.READ_EXTERNAL_STORAGE;
+        int writePermissionResult = ContextCompat.checkSelfPermission( getApplicationContext() , writePermission);
+        int readPermissionResult = ContextCompat.checkSelfPermission( getApplicationContext(), readPermission);
+        return writePermissionResult == PackageManager.PERMISSION_GRANTED && readPermissionResult == PackageManager.PERMISSION_GRANTED;
     }
 }
